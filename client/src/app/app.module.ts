ca0dd13616f835bf96ef57e143a3b67df1ac5441
nirnoy <noynir@gmail.com>
2016-09-10 19:18:23 +0300

    Revert "Merged step1_components_setup into step1_component"
    This reverts commit 6b20c1f48a4873686f5fc5b913dba8ef537d2934.

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core'
+import {AppComponent} from "./app";
+import {BrowserModule} from "@angular/platform-browser";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports     : [BrowserModule],
+  providers   : [],
+  bootstrap   : [AppComponent]
+})
+export class AppModule {
+
+}
