58677080f3bcb312bb10e6bf91b2997ec9c4ba8b
nirnoy <noynir@gmail.com>
2016-09-11 23:15:43 +0300

    step5_http_setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,13 +3,15 @@ import {AppComponent} from "./app";
 import { MoviesListComponent } from './movies/moviesList.component';
 import {MovieDetailsComponent} from './movies/movieDetails.component';
 import { MoviesService } from './common/services/movies.service';
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { MovieData } from './common/data/movies.data';
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [AppComponent,MoviesListComponent,MovieDetailsComponent],
-  imports     : [BrowserModule,FormsModule],
+  imports     : [BrowserModule,FormsModule,InMemoryWebApiModule.forRoot(MovieData)],
   providers   : [MoviesService],
   bootstrap   : [AppComponent]
 })
